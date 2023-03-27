e346d939509aa6ae91be18d8120a99c450e8bf04
gdiamadis <diamantisgiorgos2@gmail.com>
2023-03-27 14:13:26 +0300

    Digital trends

--- src/components/Post.js
+++ src/components/Post.js
@@ -7,12 +7,13 @@ export default function Post({ id, categories, date, postId, slug, title, author
         key={id}
         variants={fadeIn('up', 'spring', index * .5, 1)}
         className={'flex md:flex-row flex-col gap-4'}>
-        <img src={featuredImage.sourceUrl} alt={featuredImage.altText}
+        <img src={featuredImage &&  featuredImage.sourceUrl} alt={featuredImage && featuredImage.altText}
              className={'md:w-[270px] w-full h-[250px] rounded-[32px] object-cover'} />
 
         <div className={'w-full flex justify-between items-center'}>
             <div className='flex-1 md:ml-[62px] flex flex-col max-w-[650px]'>
                 <h4>{title}</h4>
+
                 <div dangerouslySetInnerHTML={{ __html: excerpt }}></div>
             </div>
         </div>
