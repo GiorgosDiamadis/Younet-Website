de93de349f91d9c84d6651d5ab667d1bd36fcd61
gdiamadis <diamantisgiorgos2@gmail.com>
2023-03-30 20:13:29 +0300

    Responsive home page

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 import useSite from 'hooks/use-site'
@@ -14,7 +14,12 @@ import NavListItem from '../NavListItem'
 const Nav = () => {
     const menuRef = useRef()
     const scrollPosition = useScrollPosition()
+    const [width, setWidth] = useState()
 
+    useEffect(() => {
+
+        setWidth(window.screen.width)
+    }, [])
     const { metadata = {}, menus } = useSite()
 
     const { title } = metadata
@@ -55,7 +60,7 @@ const Nav = () => {
                      className={'p-[1rem] main-nav pt-2 fixed m-0 w-full z-50'}>
                     <div className='gradient-01 h-[239px] absolute w-[152px]' />
                     <div className={'site-navigation'}>
-                        <p  className={styles.navName + ' z-50'}>
+                        <p className={styles.navName + ' z-50'}>
                             <Link legacyBehavior href='/'>
                                 <a className={'text-4xl text-white p-0 m-0'}>
                                     <Image src={logo} alt='me' width={'90'} height={'51'} loading={'lazy'} />
@@ -63,29 +68,43 @@ const Nav = () => {
                                 </a>
                             </Link>
                         </p>
-                        <div  className={'flex gap-6'}>
+                        <div className={'flex gap-6'}>
                             <button aria-label={'Ζητήστε τιμή'} type={'button'} className={'burger-container'}>
                                 <CallToAction>
                                     <div data-menu-transition className={'request-quote hidden md:block'}>Ζητηστε τιμη
                                     </div>
                                 </CallToAction>
                             </button>
-                            <button aria-label={'Μενού'} type={'button'} className={'burger-container'} onClick={() => {
-                                const lines = [...document.querySelectorAll('[data-menu-transition]')]
-                                lines.forEach((line) => {
-                                    line.classList.toggle('menu-open')
-                                })
-                            }} onTouchStart={() => {
-                                const lines = [...document.querySelectorAll('[data-menu-transition]')]
-                                lines.forEach((line) => {
-                                    line.classList.toggle('menu-open')
-                                })
-                            }}>
-                                <div id={'nav-burger'} className={'flex gap-2 relative'}>
-                                    <Burger data-menu-transition />
-                                    <h1 data-menu-transition className={'menu-text hidden md:block'}>menu</h1>
-                                </div>
-                            </button>
+                            {width > 800 ? <>
+                                <button aria-label={'Μενού'} type={'button'} className={'burger-container'}
+                                        onClick={() => {
+                                            const lines = [...document.querySelectorAll('[data-menu-transition]')]
+                                            lines.forEach((line) => {
+                                                line.classList.toggle('menu-open')
+                                            })
+                                        }}>
+                                    <div id={'nav-burger'} className={'flex gap-2 relative'}>
+                                        <Burger data-menu-transition />
+                                        <h1 data-menu-transition className={'menu-text hidden md:block'}>menu</h1>
+                                    </div>
+                                </button>
+
+                            </> : <>
+
+                                <button aria-label={'Μενού'} type={'button'} className={'burger-container'}
+                                        onTouchStart={() => {
+                                            const lines = [...document.querySelectorAll('[data-menu-transition]')]
+                                            lines.forEach((line) => {
+                                                line.classList.toggle('menu-open')
+                                            })
+                                        }}>
+                                    <div id={'nav-burger'} className={'flex gap-2 relative'}>
+                                        <Burger data-menu-transition />
+                                        <h1 data-menu-transition className={'menu-text hidden md:block'}>menu</h1>
+                                    </div>
+                                </button>
+                            </>}
+
                         </div>
                     </div>
                 </nav>
