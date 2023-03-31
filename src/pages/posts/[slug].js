ebf003c3f23a35408a9b882445d33ba5e8df1014
gdiamadis <diamantisgiorgos2@gmail.com>
2023-03-31 12:39:00 +0300

    Archive pages

--- src/pages/posts/[slug].js
+++ src/pages/posts/[slug].js
@@ -13,8 +13,9 @@ import Container from 'components/Container'
 import Content from 'components/Content'
 import Metadata from 'components/Metadata'
 import FeaturedImage from 'components/FeaturedImage'
+import Image from 'next/image'
 
-export default function Post({post, socialImage, related}) {
+export default function Post({ post, socialImage, related }) {
     const {
         title,
         metaTitle,
@@ -25,62 +26,62 @@ export default function Post({post, socialImage, related}) {
         categories,
         modified,
         featuredImage,
-        isSticky = false,
-    } = post;
+        isSticky = false
+    } = post
 
-    const {metadata: siteMetadata = {}, homepage} = useSite();
+    const { metadata: siteMetadata = {}, homepage } = useSite()
 
     if (!post.og) {
-        post.og = {};
+        post.og = {}
     }
 
-    post.og.imageUrl = `${homepage}${socialImage}`;
-    post.og.imageSecureUrl = post.og.imageUrl;
-    post.og.imageWidth = 2000;
-    post.og.imageHeight = 1000;
+    post.og.imageUrl = `${homepage}${socialImage}`
+    post.og.imageSecureUrl = post.og.imageUrl
+    post.og.imageWidth = 2000
+    post.og.imageHeight = 1000
 
-    const {metadata} = usePageMetadata({
+    const { metadata } = usePageMetadata({
         metadata: {
             ...post,
             title: metaTitle,
-            description: description || post.og?.description || `Read more about ${title}`,
-        },
-    });
+            description: description || post.og?.description || `Read more about ${title}`
+        }
+    })
 
     if (process.env.WORDPRESS_PLUGIN_SEO !== true) {
-        metadata.title = `${title} - ${siteMetadata.title}`;
-        metadata.og.title = metadata.title;
-        metadata.twitter.title = metadata.title;
+        metadata.title = `${title} - ${siteMetadata.title}`
+        metadata.og.title = metadata.title
+        metadata.twitter.title = metadata.title
     }
 
     const metadataOptions = {
-        compactCategories: false,
-    };
+        compactCategories: false
+    }
 
-    const {posts: relatedPostsList, title: relatedPostsTitle} = related || {};
+    const { posts: relatedPostsList, title: relatedPostsTitle } = related || {}
 
-    const helmetSettings = helmetSettingsFromMetadata(metadata);
+    const helmetSettings = helmetSettingsFromMetadata(metadata)
 
     return (
         <Layout classes={'bg-white'}>
             <Helmet {...helmetSettings} />
 
-            <ArticleJsonLd post={post} siteTitle={siteMetadata.title}/>
+            <ArticleJsonLd post={post} siteTitle={siteMetadata.title} />
 
             {featuredImage && (
-                <FeaturedImage
-                    {...featuredImage}
-                    src={featuredImage.sourceUrl}
-                    dangerouslySetInnerHTML={featuredImage.caption}
-                />
+                <div className={'h-[30%] overflow-hidden'}>
+
+                    <Image alt={''} width={1920} height={400} className={'w-full h-auto'}
+                           src={featuredImage.sourceUrl} />
+                </div>
             )}
             <Content>
                 <Section>
                     <Container>
                         <h1
-                            className={"text-8xl m-auto text-center"}
+                            className={'text-8xl m-auto text-center'}
                             dangerouslySetInnerHTML={{
-                                __html: title,
+                                __html: title
                             }}
                         />
                         <Metadata
@@ -92,9 +93,9 @@ export default function Post({post, socialImage, related}) {
                             isSticky={isSticky}
                         />
                         <div
-                            className={"wp-content"}
+                            className={'wp-content'}
                             dangerouslySetInnerHTML={{
-                                __html: content,
+                                __html: content
                             }}
 
                         >
@@ -105,62 +106,62 @@ export default function Post({post, socialImage, related}) {
             </Content>
 
         </Layout>
-    );
+    )
 }
 
-export async function getStaticProps({params = {}} = {}) {
-    const {post} = await getPostBySlug(params?.slug);
+export async function getStaticProps({ params = {} } = {}) {
+    const { post } = await getPostBySlug(params?.slug)
 
     if (!post) {
         return {
             props: {},
-            notFound: true,
-        };
+            notFound: true
+        }
     }
 
-    const {categories, databaseId: postId} = post;
+    const { categories, databaseId: postId } = post
 
     const props = {
         post,
-        socialImage: `${process.env.OG_IMAGE_DIRECTORY}/${params?.slug}.png`,
-    };
+        socialImage: `${process.env.OG_IMAGE_DIRECTORY}/${params?.slug}.png`
+    }
 
-    const {category: relatedCategory, posts: relatedPosts} = (await getRelatedPosts(categories, postId)) || {};
-    const hasRelated = relatedCategory && Array.isArray(relatedPosts) && relatedPosts.length;
+    const { category: relatedCategory, posts: relatedPosts } = (await getRelatedPosts(categories, postId)) || {}
+    const hasRelated = relatedCategory && Array.isArray(relatedPosts) && relatedPosts.length
 
     if (hasRelated) {
         props.related = {
             posts: relatedPosts,
             title: {
                 name: relatedCategory.name || null,
-                link: categoryPathBySlug(relatedCategory.slug),
-            },
-        };
+                link: categoryPathBySlug(relatedCategory.slug)
+            }
+        }
     }
     return {
         props,
-        revalidate: 5,
-    };
+        revalidate: 5
+    }
 }
 
 export async function getStaticPaths() {
 
-    const {posts} = await getRecentPosts({
+    const { posts } = await getRecentPosts({
         count: process.env.POSTS_PRERENDER_COUNT, // Update this value in next.config.js!
-        queryIncludes: 'index',
-    });
+        queryIncludes: 'index'
+    })
 
     const paths = posts
-        .filter(({slug}) => typeof slug === 'string')
-        .map(({slug}) => ({
+        .filter(({ slug }) => typeof slug === 'string')
+        .map(({ slug }) => ({
             params: {
-                slug,
-            },
-        }));
+                slug
+            }
+        }))
 
 
     return {
         paths,
-        fallback: 'blocking',
-    };
+        fallback: 'blocking'
+    }
 }
