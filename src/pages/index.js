e89998e5e60226f52eba7c286f4feee18bd9a102
gdiamadis <diamantisgiorgos2@gmail.com>
2023-04-06 15:54:20 +0300

    Homepage uses splide

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,13 @@
-import Layout from "components/Layout";
-import React, {useEffect, useRef, useState} from "react";
-import useEventListener from "hooks/useEventListener";
-import dynamic from "next/dynamic";
-import {getBrands, getProjects, getRecentPosts, getServices} from "../../lib/posts";
-import useEffectOnce from "hooks/useEffectOnce";
+import Layout from 'components/Layout'
+import React from 'react'
+import dynamic from 'next/dynamic'
+import { getBrands, getProjects, getServices } from 'lib/posts'
 
-import {Splide, SplideSlide} from '@splidejs/react-splide';
-import Header from "components/Header";
-import SectionTitle from "components/SectionTitle";
-import CallToActionFramerHOC from "components/CallToActionFramerHOC";
-import ContactForm from "components/ContactForm";
-import LatestPosts from "components/LatestPosts";
+import { Splide, SplideSlide } from '@splidejs/react-splide'
+import Header from 'components/Header'
+import SectionTitle from 'components/SectionTitle'
+import CallToActionFramerHOC from 'components/CallToActionFramerHOC'
+import ContactForm from 'components/ContactForm'
 
 const DynamicPortfolio = dynamic(() => import('components/Younet/Portfolio'), {
     loading: () => 'Loading'
@@ -25,69 +22,15 @@ const DynamicNewsletter = dynamic(() => import('components/Younet/NewsLetter'), 
     loading: () => 'Loading'
 })
 
-const DynamicLatestPosts = dynamic(() => import('components/LatestPosts'), {
-    loading: () => 'Loading'
-})
-export default function Home({services, brands, projectData, posts}) {
-
-    // how many tabs
-    const [numTabs, setNumTabs] = useState(0)
-    const [tabIndex, setTabIndex] = useState(1)
-    const [canScroll, setCanScroll] = useState(true)
-
-    useEventListener('wheel', (e) => {
-        if (!canScroll) return
-
-        if (e.deltaY > 0) {
-            if (tabIndex < numTabs) {
-                setTabIndex(tabIndex + 1)
-                document.querySelector("footer").classList.add("hidden")
-            } else if (tabIndex === numTabs) {
-                document.querySelector("footer").classList.remove("hidden")
-            }
-        } else {
-            if (tabIndex > 1) {
-                document.querySelector("footer").classList.add("hidden")
-                setTabIndex(tabIndex - 1)
-            }
-        }
-
-        setCanScroll(false)
-        setTimeout(() => {
-            setCanScroll(true)
-        }, 1000)
-    })
-
-    useEffectOnce(() => {
-        const tabs = document.querySelectorAll(".tabcontent")
-        if (tabs) {
-            setNumTabs(tabs.length)
-        }
-    })
-
-
-    useEffect(() => {
-        if (document === undefined) return
-        const tab = document.querySelector(`#tab${tabIndex}`)
-        if (tab) {
-            tab.classList.add("active")
-            const tabs = document.querySelectorAll(`.tabcontent:not(#tab${tabIndex})`)
-            if (tabs) {
-                tabs.forEach((el) => {
-                    el.classList.remove("active")
-                })
-            }
-
-        }
-    }, [tabIndex])
+export default function Home({ services, brands, projectData }) {
 
 
     return <Layout classes={'bg-gradient-body'}>
 
-        <div className=" absolute top-0 left-0 right-0 bottom-0 h-[100vh] flex justify-center items-center">
-            <Splide aria-label="My Favorite Images" options={{
-                direction: "ttb",
-                height: "100vh",
+        <div className=' absolute top-0 left-0 right-0 bottom-0 h-[100vh] flex justify-center items-center'>
+            <Splide aria-label='My Favorite Images' options={{
+                direction: 'ttb',
+                height: '100vh',
                 wheel: true,
                 wheelSleep: 200,
                 speed: 800
@@ -96,7 +39,7 @@ export default function Home({services, brands, projectData, posts}) {
                     <Header>
                         <div className='header-content flex flex-col items-center overflow-y-hidden'>
                             <SectionTitle title={'Growing Brands Online'}
-                                          subTitle={'Results-Driven Creative Digital Agency Focused On'}/>
+                                          subTitle={'Results-Driven Creative Digital Agency Focused On'} />
 
                             <CallToActionFramerHOC className={'request-quote cursor-pointer'} onClick={() => {
                             }}>
@@ -106,49 +49,33 @@ export default function Home({services, brands, projectData, posts}) {
                     </Header>
                 </SplideSlide>
                 <SplideSlide className={'overflow-y-hidden flex flex-col items-center'}>
-                    <DynamicServices services={services}/>
+                    <DynamicServices services={services} />
                 </SplideSlide>
                 <SplideSlide className={'overflow-y-hidden flex flex-col items-center'}>
                     <DynamicPortfolio projects={projectData.projects} projectCategories={projectData.categories}
-                                      category={8}/>
+                                      category={8} />
                 </SplideSlide>
                 <SplideSlide className={'overflow-y-hidden flex flex-col items-center'}>
-                    <DynamicBrands brands={brands}/>
+                    <DynamicBrands brands={brands} />
                 </SplideSlide>
                 <SplideSlide className={'overflow-y-hidden flex flex-col items-center'}>
-                    <LatestPosts posts={posts}/>
-                </SplideSlide>
-                <SplideSlide className={'overflow-y-hidden flex flex-col items-center'}>
-                    <ContactForm/>
+                    <ContactForm />
                 </SplideSlide>
 
                 <SplideSlide className={'overflow-y-hidden flex flex-col items-center'}>
-                    <DynamicNewsletter/>
+                    <DynamicNewsletter />
                 </SplideSlide>
             </Splide>
         </div>
 
-        {/*<div id="tab1" className="tabcontent active " style={{height: `calc(100vh)`}}>*/}
-
-
-        {/*</div>*/}
-
-        {/*<div id="tab2" className="tabcontent " style={{height: `calc(100vh)`}}>*/}
-
-        {/*</div>*/}
     </Layout>
 }
 
 export async function getStaticProps() {
 
-    const {services} = await getServices(13)
-    const {brands} = await getBrands()
-    const {projects, categories} = await getProjects(8)
-
-    const {posts} = await getRecentPosts({
-        count: 3,
-        queryIncludes: 'archive'
-    })
+    const { services } = await getServices(13)
+    const { brands } = await getBrands()
+    const { projects, categories } = await getProjects(8)
 
     return {
         revalidate: 5,
@@ -158,8 +85,7 @@ export async function getStaticProps() {
             projectData: {
                 projects,
                 categories
-            },
-            posts
+            }
         }
     }
 }
